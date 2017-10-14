6d9786fdcc06c88c5d672a411529937fd9af57f9
largeslurpee <nelsontaylor@gatech.edu>
2017-10-14 02:40:05 -0400

    created UserClass.cs for User Class

---
+++ UserClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class User
+{
+    private string name;
+    private string userName;
+    private string password;
+    private double miles;
+    private List<string> sightsPrefs;
+    private List<User> friends;
+    private Trip currentTrip;
+    private List<Trip> trips;
+
+}
