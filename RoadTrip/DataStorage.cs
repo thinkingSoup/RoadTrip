94c8aff6c0325075af41e9b662c0ea1843a0e0c5
Tristen Allen <spyboticsguy@gmail.com>
2017-10-14 09:28:12 -0400

    added data storage class

---
+++ RoadTrip/DataStorage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Microsoft.WindowsAzure.MobileServices;
+namespace RoadTrip
+{
+    public class DataStorage
+    {
+        private static readonly DataStorage instance = new DataStorage();
+        public MobileServiceClient client { get; }
+        private DataStorage()
+        {
+            client = new MobileServiceClient("https://thinkingsoup-roadtrip.azurewebsites.net");
+        }
+
+        public static DataStorage Instance {
+            get {
+                return instance;
+            }
+        }
+    }
+}
