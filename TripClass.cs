88d4d224383357b7f8dd1a7afa29e53c73f41326
largeslurpee <nelsontaylor@gatech.edu>
2017-10-14 03:11:19 -0400

    Added Getters and Setters for Trip and User Class

--- TripClass.cs
+++ TripClass.cs
@@ -3,8 +3,31 @@ using System;
 public class Trip
 {
     private string tripName;
+    public string TripName
+    {
+        get { return tripName; }
+        set { tripName = values; }
+    }
+
     private double distance;
+    public double Distance
+    {
+        get { return distance; }
+        set { distance = value; }
+    }
+
     private double days;
+    public double Days
+    {
+        get { return days; }
+        set { days = value; }
+    }
+
     private float price;
+    public float Price
+    {
+        get { return price; }
+        set { days = value; }
+    }
 
 }
