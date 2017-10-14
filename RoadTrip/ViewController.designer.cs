49bdf0086a660cef9861fe36ebf7367f4238c26c
Allan Serna <allanserna@gatech.edu>
2017-10-14 03:34:17 -0400

    * LaunchScreen.storyboard: UI Changes to the start screen
    * Info.plist:
    * RoadTrip.csproj:
    * ViewController.designer.cs:

--- RoadTrip/ViewController.designer.cs
+++ RoadTrip/ViewController.designer.cs
@@ -1,17 +1,37 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 
 namespace RoadTrip
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView startImage { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextView startLabel { get; set; }
+
+        void ReleaseDesignerOutlets ()
         {
+            if (startImage != null) {
+                startImage.Dispose ();
+                startImage = null;
+            }
+
+            if (startLabel != null) {
+                startLabel.Dispose ();
+                startLabel = null;
+            }
         }
     }
 }
