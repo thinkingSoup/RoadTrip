189e837b6717b767b43eaca26c2592301eab1892
Tristen Allen <spyboticsguy@gmail.com>
2017-10-14 19:41:59 -0400

    change userclass for database

--- RoadTrip/Model/UserClass.cs
+++ RoadTrip/Model/UserClass.cs
@@ -1,38 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 namespace RoadTrip.Model
 {
     public class User
     {
-        private string name { get; set; }
-        private string userName { get; set; }
-        private string password { get; set; }
-        private double miles { get; set; }
-        private List<string> sightsPrefs { get; set; }
-        private List<User> friends { get; set; }
-        private Trip currentTrip { get; set; }
-        private List<Trip> trips { get; set; }
-
-        public User(string _name, string _userName, string _password, double _miles, List<string> _sightsPrefs, List<User> _friends, Trip _currentTrip, List<Trip> _trips)
-        {
-            name = _name;
-            userName = _userName;
-            password = _password;
-            miles = _miles;
-            sightsPrefs = _sightsPrefs;
-            friends = _friends;
-            currentTrip = _currentTrip;
-            trips = _trips;
-        }
+        public string id { get; set; }
+        public string name { get; set; }
+        public double miles { get; set; }
+        public List<string> sightsPrefs { get; set; }
+        //private List<User> friends { get; set; }
+        public int currentTrip { get; set; }
+        //private List<Trip> trips { get; set; }
 
         public void incrementMiles(double dist)
         {
             miles = miles + dist;
         }
 
-        public void addFriend(User person)
+        /*public void addFriend(User person)
         {
             friends.Add(person);
+        }*/
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
         }
 
     }
