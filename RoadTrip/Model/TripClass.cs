e5ab5ba69a9a5056c3459fa5ff97fe66d4f89050
largeslurpee <nelsontaylor@gatech.edu>
2017-10-15 03:09:30 -0400

    Fixed the New Trip newRoute function to actually be C# code

--- RoadTrip/Model/TripClass.cs
+++ RoadTrip/Model/TripClass.cs
@@ -26,24 +26,7 @@ namespace RoadTrip.Model
         public MKRoute route { get; set; }
         public List<Checkin> checkIns { get; set; }
 
-        public MKRoute newRoute (MKMapItem start, MKMapItem finish)
-        {
-            MKDirectionsRequest *drivingRouteRequest = [[MKDirectionsRequest alloc] init];
-            drivingRouteRequest.transportType = MKDirectionsTransportTypeAutomobile;
-            [drivingRouteRequest setSource:[start]];
-            [drivingRouteRequest setDestination:[finish]];
-
-            MKDirections *drivingRouteDirections = [[MKDirections alloc] initWithRequest:drivingRouteRequest];
-            [drivingRouteDirections calculateDirectionsWithCompletionHandler:^(MKDirectionsResponse * drivingRouteResponse, NSError *drivingRouteError) {
-            if (drivingRouteError) {
-                [self handleDirectionsError:drivingRouteError];
-            } else {
-                // The code doesn't request alternate routes, so add the single calculated route to
-                // a previously declared MKRoute property called walkingRoute.
-                self.drivingRoute = drivingRouteResponse.routes[0];
-            }
-        }];
-        }
+        
 
         public override string ToString()
         {
